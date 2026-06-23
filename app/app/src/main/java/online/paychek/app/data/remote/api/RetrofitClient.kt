9069288a3786695dabeb3eb5e7d46f3c78284cb0
Paychek Dev <dev@paychek.local>
2026-06-23 17:07:34 +0600

    fix: use custom BooleanDeserializer in RetrofitClient to handle NUMBER as boolean from backend

--- app/app/src/main/java/online/paychek/app/data/remote/api/RetrofitClient.kt
+++ app/app/src/main/java/online/paychek/app/data/remote/api/RetrofitClient.kt
@@ -5,7 +5,33 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import java.lang.reflect.Type
+
+class BooleanDeserializer : JsonDeserializer<Boolean> {
+    override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): Boolean {
+        if (json.isJsonPrimitive) {
+            val primitive = json.asJsonPrimitive
+            if (primitive.isBoolean) return primitive.asBoolean
+            if (primitive.isNumber) return primitive.asInt == 1
+            if (primitive.isString) {
+                val str = primitive.asString
+                return str.equals("true", ignoreCase = true) || str == "1"
+            }
+        }
+        return false
+    }
+}
+
 object RetrofitClient {
+
+    private val customGson = GsonBuilder()
+        .registerTypeAdapter(Boolean::class.java, BooleanDeserializer())
+        .registerTypeAdapter(Boolean::class.javaObjectType, BooleanDeserializer())
+        .create()
 
     private val okHttpClient: OkHttpClient by lazy {
         OkHttpClient.Builder()
@@ -22,7 +48,7 @@ object RetrofitClient {
         Retrofit.Builder()
             .baseUrl(AppConfig.API_BASE_URL)
             .client(okHttpClient)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(customGson))
             .build()
             .create(AuthApiService::class.java)
     }
@@ -31,7 +57,7 @@ object RetrofitClient {
         Retrofit.Builder()
             .baseUrl(AppConfig.API_BASE_URL)
             .client(okHttpClient)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(customGson))
             .build()
             .create(PaymentApiService::class.java)
     }
@@ -40,7 +66,7 @@ object RetrofitClient {
         Retrofit.Builder()
             .baseUrl(AppConfig.API_BASE_URL)
             .client(okHttpClient)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(customGson))
             .build()
             .create(GatewayApiService::class.java)
     }
@@ -49,7 +75,7 @@ object RetrofitClient {
         Retrofit.Builder()
             .baseUrl(AppConfig.API_BASE_URL)
             .client(okHttpClient)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(customGson))
             .build()
             .create(AdminApiService::class.java)
     }
@@ -58,7 +84,7 @@ object RetrofitClient {
         Retrofit.Builder()
             .baseUrl(AppConfig.API_BASE_URL)
             .client(okHttpClient)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(customGson))
             .build()
             .create(ProfileApiService::class.java)
     }
